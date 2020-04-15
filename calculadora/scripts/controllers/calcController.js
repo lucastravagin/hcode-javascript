b2f7e3bfaef5b7b242e465f9afc2b5561c7f8f50
Lucas Travagin <lucas_travagin1112@hotmail.com>
2020-04-14 23:22:26 -0300

    Aplicando vários eventos utilizando o split

--- calculadora/scripts/controllers/calcController.js
+++ calculadora/scripts/controllers/calcController.js
@@ -20,14 +20,26 @@ class CalcController {
         }, 1000)
     }
 
+    addEventListenerAll(element, events, fn) {
+        events.split(' ').forEach(event => {
+            element.addEventListener(event, fn, false)
+        })
+    }
+
     initiButtonsEvents() {
         let buttons = document.querySelectorAll("#buttons > g, #parts > g")
 
         buttons.forEach((btn) => {
-            btn.addEventListener('click', e => {
+            this.addEventListenerAll(btn, 'click drag', e => {
                 console.log(btn.className.baseVal.replace("btn-", ""))
             })
+
+            this.addEventListenerAll(btn, "mouseover mouseup mousedown", e => {
+                btn.style.cursor = "pointer"
+            })
         })
+
+
     }
 
     setDisplatDateTime() {
