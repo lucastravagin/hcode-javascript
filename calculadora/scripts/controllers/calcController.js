980f1821b56103b464acc60978ea5bed596132da
Lucas Travagin <lucas_travagin1112@hotmail.com>
2020-04-21 18:19:15 -0300

    Clicando mais de uma vez no botao igual

--- calculadora/scripts/controllers/calcController.js
+++ calculadora/scripts/controllers/calcController.js
@@ -5,6 +5,9 @@ class CalcController {
     //Método construtor
     constructor() {
 
+        this._lastOperator = ''
+        this._lastNumber = ''
+
         this._operation = []
         this._displayCalcEl = document.querySelector('#display')
         this._dateEl = document.querySelector('#data')
@@ -66,28 +69,65 @@ class CalcController {
         }
     }
 
+    getResult() {
+        return eval(this._operation.join(""))
+    }
+
     calc() {
 
         let last = ''
 
-        if(this._operation.length > 3) {
-            last = this._operation.pop()
+        this._lastOperator = this.getLastItem()
+
+        if (this._operation.length < 3) {
+
+            let firstItem = this._operation[0]
+            this._operation = [firstItem, this._lastOperator, this._lastNumber]
         }
 
-        let result = eval(this._operation.join(""))
+        if (this._operation.length > 3) {
+            last = this._operation.pop()
+
+            
+            this._lastNumber = this.getResult()
+
+        } else if(this._operation.length == 3) {
+            this._lastNumber = this.getLastItem(false)
+        }
+
+
+
+        let result = this.getResult()
         if (last == '%') {
 
             result /= 100
 
             this._operation = [result]
         } else {
-            
+
             this._operation = [result]
 
             if (last) this._operation.push(last)
         }
 
         this.setLastNumberToDisplay()
+    }
+
+    getLastItem(isOperator = true) {
+        let lastItem;
+
+        for (let i = this._operation.length - 1; i >= 0; i--) {
+
+            if (this.isOperator(this._operation[i]) == isOperator) {
+                lastItem = this._operation[i]
+                break
+            }
+        }
+
+        if(!lastItem) {
+            lastItem = (isOperator) ? this._lastOperator : this._lastNumber
+        }
+        return lastItem
     }
 
     setLastNumberToDisplay() {
@@ -102,7 +142,7 @@ class CalcController {
             }
         }
 
-        if(!lastNumber) lastNumber = 0
+        if (!lastNumber) lastNumber = 0
         this.displayCalc = lastNumber
 
     }
