9eeae84886be1695937e9451e45f39808038f0d7
Lucas Travagin <lucas_travagin1112@hotmail.com>
2020-04-24 00:25:28 -0300

    Colocando audio na calculadora

--- calculadora/scripts/controllers/calcController.js
+++ calculadora/scripts/controllers/calcController.js
@@ -5,6 +5,9 @@ class CalcController {
     //Método construtor
     constructor() {
 
+        this._audio = new Audio('click.mp3')
+        this._audioOnOff = false
+
         this._lastOperator = ''
         this._lastNumber = ''
 
@@ -53,11 +56,35 @@ class CalcController {
         this.setLastNumberToDisplay()
 
         this.pasteFromClipboard()
+
+        document.querySelectorAll('.btn-ac').forEach( btn => {
+
+            btn.addEventListener('dblclick', e=> {
+                console.log('Ativou o audio')
+                this.toggleAudio()
+            })
+        })
+    }
+
+    toggleAudio() {
+
+        this._audioOnOff = !this._audioOnOff
+    }
+
+    playAudio() {
+
+        if (this._audioOnOff) {
+
+            this._audio.currentTime = 0
+            this._audio.play()
+        }
     }
 
     initKeyboard() {
 
         document.addEventListener('keyup', e => {
+
+            this.playAudio()
             switch (e.key) {
 
                 case 'Escape':
@@ -270,6 +297,9 @@ class CalcController {
     }
 
     execBtn(value) {
+
+        this.playAudio()
+
         switch (value) {
 
             case 'ac':
